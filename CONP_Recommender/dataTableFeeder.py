3eb20971c800cd4f3f840673e39ff70034d5dc4a
Mandana <46631831+mandana-mazaheri@users.noreply.github.com>
2020-07-09 14:11:51 -0400

    Add files via upload

--- CONP_Recommender/dataTableFeeder.py
+++ CONP_Recommender/dataTableFeeder.py
@@ -5,18 +5,20 @@ import os.path
 from os import path
 import sqlite3
 import csv
+from os import stat
 
 class dataTableFeeder(object):
 
-    def fileCount(self, folder):
+    def symlinkCount(self, folder):
         count = 0
         for filename in os.listdir(folder):
             path = os.path.join(folder, filename)
 
-            if os.path.isfile(path):
+            #if os.path.isfile(path):
+            if os.path.islink(path):
                 count += 1
             elif os.path.isdir(path):
-                count += self.fileCount(path)
+                count += self.symlinkCount(path)
         return count
 
 
@@ -41,7 +43,7 @@ class dataTableFeeder(object):
 
         list_of_datasets_path = self.findDataset(root)
         conn = sqlite3.connect(os.path.join(cachePath, 'CONP.db'))
-        HASH_KEYWORDS = ["/MD5E-"]
+        HASH_KEYWORDS = ["MD5E-"]
         # HASH_KEYWORDS = ["/MD5E-", "/URL"]
 
         # conn.execute("PRAGMA foreign_keys = 1")
@@ -65,72 +67,73 @@ class dataTableFeeder(object):
         numOfFilesToProcess = 0
         numOfProcessedFiles = 0
         for dataset_path in list_of_datasets_path:
-            numOfFilesToProcess += self.fileCount(dataset_path)
+            numOfFilesToProcess += self.symlinkCount(dataset_path)
+            #print(self.symlinkCount(dataset_path))
         
-        HASH_KEYWORDS = ["/MD5E-"]
+        keyDetected = 0    
+        HASH_KEYWORDS = ["MD5E-"]
         for dataset_path in list_of_datasets_path:
             #dataset_name = str(dataset_path).split('\\')[-1]
             dataset_name = str(dataset_path).split(os.path.sep)[-1]
             #print(dataset_name)
             for path, subdirs, files in os.walk(dataset_path):
                 for name in files:
+                    #print(name)
+                    path_fileName = os.path.join(path, name)
+                    
+                    #if os.path.islink(path_fileName):
+                    #    print(os.readlink(path_fileName))
 
-                    numOfProcessedFiles += 1
-                    print("\r" + str(numOfProcessedFiles) + " file of "+ str(numOfFilesToProcess) + " processed (" + str(int(numOfProcessedFiles * 100 / numOfFilesToProcess)) + "%)", end='')
+                    if os.path.islink(path_fileName):
+                        numOfProcessedFiles += 1
+                        print("\r" + str(numOfProcessedFiles) + " file of "+ str(numOfFilesToProcess) + " processed (" + str(int(numOfProcessedFiles * 100 / numOfFilesToProcess)) + "%)", end='')
 
-                    if os.path.getsize(os.path.join(path, name)) < 2000:
+                        #if os.path.getsize(os.path.join(path, name)) < 2000:
+                        file_content = os.readlink(path_fileName)
+                        #print(file_content)  
+
                         list_to_insert = []
-                        with open(os.path.join(path, name), 'r') as file:
-                            try:
-                                file_content = file.read()
-                                file.close()
-                                for hash_key in HASH_KEYWORDS:
-                                    if hash_key in file_content:
-                                        # print (os.path.join(path, name))
-                                        path_fileName = os.path.join(path, name)
-                                        # print("___________"+path_fileName, flush = True)
+                        
+                        for hash_key in HASH_KEYWORDS:
+                            if hash_key in file_content:
+                                #keyDetected += 1
+                                #print(keyDetected)
+                                
+                                file_name = (path_fileName.split(os.path.sep + "projects" + os.path.sep)[1]).split(os.path.sep)[-1]
 
-                                        # dataset_name = (path_fileName.split("\\projects\\")[1]).split("\\")[0]
+                                # print(file_content)
+                                string_lists = file_content.split("/")
+                                # print("string_lists_", flush = True)
+                                # print(string_lists)
+                                if dataset_name and file_name:
+                                    for string in string_lists:
+                                        if hash_key.replace('/', '') in string:
+                                            # print(string)
+                                            string = string.replace('--', '-')
+                                            sub_strings = string.split("-")
+                                            # print(sub_strings)
+                                            hash_function = sub_strings[0]
+                                            # print("Fnction : " + hash_function)
 
-                                        # print("Dataset Name:    "+dataset_name)
-                                        file_name = (path_fileName.split(os.path.sep + "projects" + os.path.sep)[1]).split(os.path.sep)[-1]
-                                        # print("File Name:    "+file_name)
+                                            hash_value = sub_strings[2].split('.')[0]
+                                            # print("Hash Value : " + hash_value)
+                                            latest_commit = sub_strings[1]
+                                            if not latest_commit:
+                                                latest_commit = ""
 
-                                        # print(file_content)
-                                        string_lists = file_content.split("/")
-                                        # print("string_lists_", flush = True)
-                                        # print(string_lists)
-                                        if dataset_name and file_name:
-                                            for string in string_lists:
-                                                if hash_key.replace('/', '') in string:
-                                                    # print(string)
-                                                    string = string.replace('--', '-')
-                                                    sub_strings = string.split("-")
-                                                    # print(sub_strings)
-                                                    hash_function = sub_strings[0]
-                                                    # print("Fnction : " + hash_function)
+                                            # print("Commit : " + latest_commit)
+                                            try:
+                                                cursor.execute("INSERT INTO data_files VALUES (?,?,?,?,?,?);", (
+                                                count, dataset_name, file_name, hash_function, hash_value, latest_commit))
+                                                conn.commit()
+                                                # print("after database", flush = True)
+                                                count = count + 1
+                                                #print("Count : " + str(count))
+                                            except sqlite3.IntegrityError:
+                                                print("duplicated file in dataset: ", dataset_name, "\n file name: ",file_name)
 
-                                                    hash_value = sub_strings[2].split('.')[0]
-                                                    # print("Hash Value : " + hash_value)
-                                                    latest_commit = sub_strings[1]
-                                                    if not latest_commit:
-                                                        latest_commit = ""
+                                            break
 
-                                                    # print("Commit : " + latest_commit)
-                                                    try:
-                                                        cursor.execute("INSERT INTO data_files VALUES (?,?,?,?,?,?);", (
-                                                        count, dataset_name, file_name, hash_function, hash_value, latest_commit))
-                                                        conn.commit()
-                                                        # print("after database", flush = True)
-                                                        count = count + 1
-                                                        #print("Count : " + str(count))
-                                                    except sqlite3.IntegrityError:
-                                                        print("duplicated file in dataset: ", dataset_name, "\n file name: ",file_name)
-
-                                                    break
-
-                            except UnicodeDecodeError:
-                                pass
 
         conn.close()
 
